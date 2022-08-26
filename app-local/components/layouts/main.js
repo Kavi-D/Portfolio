5ff8c25046cf8cc8fe8585035f2d8fd87708c2e4
Kavi-D <kavidharmarajan3@gmail.com>
2022-08-26 16:25:28 -0400

    creating simple starting page with some information

--- app-local/components/layouts/main.js
+++ app-local/components/layouts/main.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import { Box, Container} from '@chakra-ui/react'
 
-const Main = () => {
+const Main = ({children, router }) => {
     return (
         <Box as="main" pb = {8}>
             <Head>
                 <meta name = "viewport" content="width = device-width, initial-scale =1"/>
                 <title> Kavi D - Homepage</title>
             </Head>
+
+            <Container maxW = "container.md" pt = {14}>
+                {children}
+            </Container>
         </Box>
     ) 
 }
--- app-local/pages/index.js
+++ app-local/pages/index.js
@@ -1,5 +1,29 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return (<div> Hello </div>)
+  return (
+    
+    <Container>
+      <Box borderRadius = 'lg' bg = 'green' p = {3} mb = {6} align = "center">
+         I am a Software Developer based in Canada.
+      </Box>
+
+      <Box display={ {md:'flex'}}>
+        <Box flexGrow = {1}>
+          <Heading as = "h2" variant = "page-title">
+              Kavi-D.
+          </Heading>
+        </Box>
+        <p>
+           Developer/ Data Nerd / Social Drinker
+        </p>
+      </Box>
+
+
+    </Container>
+  )
+
+
 }
 
 export default Page;
