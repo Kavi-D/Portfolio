44c3085008b86b3cde053dd38d5e91ec4770001f
Kavi-D <kavidharmarajan3@gmail.com>
2023-03-26 18:27:30 -0400

    finished RDPA page

--- app-local/pages/works/FEMA.js
+++ app-local/pages/works/FEMA.js
@@ -1,12 +1,46 @@
-import { chakra, Text, Link } from "@chakra-ui/react"
+import { chakra, Text, Link, Container } from "@chakra-ui/react"
+import Title from "../../components/title"
+import Paragraph from "../../components/Paragraph"
 
 const FEMA = () => { 
     return (
-        <Text>
-            <Link color='teal.500' href='/works'>
-            {'<-'}{' '}Back to Works
-            </Link>
-        </Text>
+
+        <Container  maxW="contianer.md" pt={14} >
+            <Text>
+                <Link color='pink.500' href='/works'>
+                {'<-'}{' '}Back to Works
+                </Link>
+
+
+                <Title>
+                    Flood Insurance Policy Estimator (2022)
+                </Title>
+
+                <Paragraph>
+                    A Backend focused application that could appropriately give US flood insurance informaiton
+                    to any building owner within 50 states.
+                </Paragraph>
+
+                <Paragraph>
+                <Text color= 'pink.200' paddingBottom='10px'> Source Code:    
+                    <Link color='pink.500' href='https://git.uwaterloo.ca/jpmascar/ece656_project_group2' isExternal>
+                        {"   "} https:git.uwaterloo.ca/kd
+                    </Link>
+
+                </Text>
+                <Text color= 'pink.200' paddingBottom='10px'> Tech Stack used:    
+                    <Link color='pink.500' isExternal>
+                        {"   "} React, HTML, CSS for basic frontend, 
+                    </Link>
+                    <Link color='pink.500' isExternal>
+                        {"   "} MySQL for backend 
+                    </Link>
+
+                </Text>
+            </Paragraph>
+            </Text>
+        </Container>
+        
     )
 }
 
--- app-local/pages/works/RDPA.js
+++ app-local/pages/works/RDPA.js
@@ -1,5 +1,5 @@
 import { chakra } from "@chakra-ui/react"
-import { Text, Link, Container } from "@chakra-ui/react"
+import { Text, Link, Container, Image} from "@chakra-ui/react"
 
 import Title from "../../components/title"
 import Paragraph from "../../components/Paragraph"
@@ -22,7 +22,7 @@ const RDPA = () => {
 
             <Paragraph>
                 <Text> Source Code:    
-                    <Link color='pink.500' href='https://keystrokeapp.com/' isExternal>
+                    <Link color='pink.500' isExternal>
                         {"   "} All rights to the software were passed to Carleton University staff and representitives.
                     </Link>
                     <br></br>
@@ -33,16 +33,35 @@ const RDPA = () => {
 
             <Paragraph>
                 <Text as='u' color='pink.400'> Purpose:</Text>
-                {" "} The project was to design and implement an algorithm that used machine learning techniques to determine if a 
+                {" "} The project was to design and implement an algorithm that used machine learning to determine if a 
                 sample of blood contained a given does of radiation.
                 <br></br>
-                The main use of the project was to develop the software for a hardware based medical device that couled be used by healthcare professionals to diagnose patients based on radiation exposure levels.
             </Paragraph>
+            <Image paddingTop="15px" borderRadius="xl" w="full" src = "/images/works/RDPA/RDPA1.JPG" alt = ''/>
             <Paragraph>
                 <Text as='u' color='pink.400'> Use Case:</Text>
                 {" "} 
-                The main use of the project was to develop the software for a hardware based medical device that couled be used by healthcare professionals to diagnose patients based on radiation exposure levels.
+                The main use of the project was to develop the software for a hardware based medical device to be used by healthcare professionals to diagnose patients based on radiation exposure levels.
             </Paragraph>
+            <Paragraph>
+            <Text as='u' color='pink.400'> Development tools used:</Text>
+            {" "}
+            Python - automation, weka - for machine learning support, MATLAB libraries for plotting and data analysis/manipulation.
+                
+            </Paragraph>
+
+            <Paragraph>
+            <Text as='u' color='pink.400'> Results:</Text>
+            {" "}
+            Overall results of the projet were deamed to be successful by lead biomedical researchers in Raman Spectroscopy. As out application could determine a given
+            blood sample dosage to a 90% confidence interval with appropriate gui and visual display that a given person can verify with
+                
+            </Paragraph>
+            <Image paddingTop="15px" borderRadius="xl" w="full" src = "/images/works/RDPA/RDPA_gui.JPG" alt = 'feed keystroke pic'/>
+            <Image paddingTop="15px" borderRadius="xl" w="full" src = "/images/works/RDPA/RDPA2.jpg" alt = 'feed keystroke pic'/>
+            <Image paddingTop="15px" borderRadius="xl" w="full" src = "/images/works/RDPA/RDPA_results.JPG" alt = 'feed keystroke pic'/>
+
+
         </Container>
 
         
