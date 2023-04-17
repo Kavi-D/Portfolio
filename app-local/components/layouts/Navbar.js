0d44644517214c47df83b549277a39bbd2d65fe1
Kavi-D <kavidharmarajan3@gmail.com>
2023-04-17 17:39:30 -0400

    changed nav bar color

--- app-local/components/layouts/Navbar.js
+++ app-local/components/layouts/Navbar.js
@@ -52,7 +52,7 @@ const LinkLayout = ({href,children, path, ...props}) => {
 const Navbar  = (props) => { 
   const { path } = props;
     return (
-        <Box position = "fixed" as="nav" w="100%" bg = {useColorModeValue('#ffffff40', '#20202380')}
+        <Box position = "fixed" as="nav" w="100%" bg = {useColorModeValue('gray200', '#20202380')}
         style = {{ backdropFilter: 'blur(10px)'}}>
           <Container display = "flex" p = {2} maxW = "container.md" wrap = "wrap" algin = "center" justify="space-between">
             <Flex align = "center" mr ={3}>
